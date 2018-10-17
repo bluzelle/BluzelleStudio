500ed9518b782736148efad869b0529ffa76366e
Monty Thibault <monty@bluzelle.com>
2018-10-17 19:44:58 -0400

    Removed heroku config

--- web/src/components/DaemonSelector/DaemonSelector.js
+++ web/src/components/DaemonSelector/DaemonSelector.js
@@ -7,33 +7,6 @@ const uuidv4 = require('uuid/v4');
 
 const url_params = window && new URLSearchParams(window.location.search);
 
-
-const requestObject = {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/vnd.heroku+json; version=3',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 74d54dfd-f692-410b-9d4b-3e239bb0d34f'
-    }
-}
-
-
-const getConfig = async appName => {
-
-    const response = await fetch('https://api.heroku.com/apps/' + appName + '/config-vars', requestObject);
-        
-    const json = response.json();
-
-    // json.BLUZELLEDB_PORT
-    // json.BLUZELLEDB_ADDRESS
-    // json.BLUZELLEDB_UUID
-
-    return json;
-
-};
-
-
-
 @observer
 export default class DaemonSelector extends Component {
     constructor(props) {
@@ -49,15 +22,14 @@ export default class DaemonSelector extends Component {
         this.uuid.value = this.uuid.value || this.uuid.placeholder;
 
 
-        if(!url_params.get('app')) {
-            !url_params.get('address') && url_params.set('address', this.address.value);
-            !url_params.get('port') && url_params.set('port', this.port.value);
-            !url_params.get('uuid') && url_params.set('uuid', this.uuid.value);
+        !url_params.get('address') && url_params.set('address', this.address.value);
+        !url_params.get('port') && url_params.set('port', this.port.value);
+        !url_params.get('uuid') && url_params.set('uuid', this.uuid.value);
 
-            const new_url_params = location.pathname + '?' + url_params.toString();
+        const new_url_params = location.pathname + '?' + url_params.toString();
 
-            window.history.pushState('', '', new_url_params);
-        }
+        window.history.pushState('', '', new_url_params);
+
 
         const ws_url = this.address.value + ':' + this.port.value;
         const uuid = this.uuid.value;
@@ -84,43 +56,6 @@ export default class DaemonSelector extends Component {
 
                 this.go();
                 return;
-
-            }
-
-            if(url_params.get('app')) {
-
-                this.setState({showConfigLoader: true});
-
-                getConfig(url_params.get('app')).then(
-                    json => {
-
-
-                        if(!('BLUZELLEDB_ADDRESS' in json)
-                            || !('BLUZELLEDB_PORT' in json)
-                            || !('BLUZELLEDB_UUID' in json)) {
-
-                            alert('Heroku config response malformed. Check console.');
-                            console.log('Heroku config response malformed. Expected "BLUZELLEDB_ADDRESS", "BLUZELLEDB_PORT", "BLUZELLEDB_UUID".');
-                            console.log(json);
-
-                            this.setState({showConfigLoader: false});
-                            return;
-
-                        }
-
-                        this.setState({showConfigLoader: false});
-
-                        this.address.value = json.BLUZELLEDB_ADDRESS;
-                        this.port.value = json.BLUZELLEDB_PORT;
-                        this.uuid.value = json.BLUZELLEDB_UUID;
-
-                        this.go();
-
-                    }, 
-                    () => {
-                        alert(`Not able to fetch config parameters for app "${url_params.get('app')}".`);
-                        this.setState({showConfigLoader: false});
-                    });
 
             }
 
